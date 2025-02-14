2cc592b615ebe4f690e755d689ac83b70ab294b5
Joao Victor Oliveira <joao.oliv.victor@gmail.com>
2025-02-14 11:37:52 -0300

    first stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @Component({
-  selector: 'xyz-root',
+  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TopoComponent,
+    HomeComponent,
+    RodapeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
